5339c166b6688f68f6b38cae898ce75d9c6bb539
Natalia Levenets <levenets.lot@gmail.com>
2023-01-10 23:09:40 +0200

    color changed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+const color = {
+    intervalID: null,
+    start() {
+        this.intervalID = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+        }, 1000)
+    },
+    stop() {
+        clearInterval(this.intervalID);
+    },
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+};
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true,
+    stopBtn.disabled = false,
+    color.start()
+})
+
+stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false,
+    stopBtn.disabled = true,
+    color.stop()
+})
 
